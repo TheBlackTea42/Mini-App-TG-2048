4359986745ad8bdbb57970fbbcb4024d901b19a9
Tea <51705220+TheBlackTea42@users.noreply.github.com>
2024-11-29 02:03:20 +0700

    bot

---
+++ 2048botstart.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+
+from aiogram.types.web_app_info import *
+API_TOKEN = "TOKEN"
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    mark = types.ReplyKeyboardMarkup()
+    mark.add(types.KeyboardButton("Открыть", web_app=WebAppInfo(url="URL")))
+    await message.answer("2048",reply_markup=mark)
+
+@dp.message_handler() 
+async def echo(message: types.Message): #Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
+   await message.answer(message.text)
+
+if __name__ == '__main__':
+   executor.start_polling(dp)
